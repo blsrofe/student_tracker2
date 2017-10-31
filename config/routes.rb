34d9ff3b18b609d9a32cb717b5d74109495f833e
Becki Srofe <blf_23@yahoo.com>
2017-10-30 22:46:43 -0600

    Add user login with test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
 
    root "welcome#index"
+
+   get '/login', to: "sessions#new"
+   post '/login', to: "sessions#create"
+   delete '/logout', to: "sessions#destroy"
+
+   get '/dashboard', to: "dashboard#index"
 end
--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -2,15 +2,17 @@ require "rails_helper"
 
 describe "As a registered user" do
   it "I can log in" do
+    user = User.create!(first_name: "Joe", last_name: "Smith", email: "joe@gmail.com", password: "password")
+
     visit '/'
-    click_on "Login"
+    click_link "Login"
 
     expect(current_path).to eq(login_path)
 
-    fill_in :email, with: "joe@gmail.com"
-    fill_in :password, with: "password"
+    fill_in "Email", with: "joe@gmail.com"
+    fill_in "Password", with: "password"
+    find('.login').click
 
-    click_on "Login"
     expect(current_path).to eq(dashboard_path)
     expect(page).to have_content("Logout")
     expect(page).to have_content("Welcome, Joe")
