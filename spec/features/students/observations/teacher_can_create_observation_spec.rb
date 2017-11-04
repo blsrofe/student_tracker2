dc14e9c8f55c5e8d5b192261d89891fd8e0116cc
Becki Srofe <blf_23@yahoo.com>
2017-11-03 21:39:55 -0600

    Add test and functionality for creating observations from student index page

---
+++ spec/features/students/observations/teacher_can_create_observation_spec.rb
@@ -0,0 +1,47 @@
+# As a teacher
+# When I visit klass/:id/students
+# And I click on "New Observation"
+# My path should be new_observation_path
+# And when I fill in data
+# And click "Create Observation"
+# My path should be /observations
+# And I should see my new observation at the end of the list
+require 'rails_helper'
+
+describe "as a teacher" do
+  it "i can create observations" do
+    user = create(:user)
+    classes = create_list(:klass, 2, user: user)
+    klass1 = Klass.first
+    klass2 = Klass.last
+    students = create_list(:student, 25)
+    klass1.students << students
+    klass2.students << students
+    student = Student.first
+    observations2 = create_list(:observation, 5, user: user)
+    student.observations << observations2
+    observation = Observation.first
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit klass_students_path(klass1)
+    within(first(".student")) do
+      click_on "New Observation"
+    end
+    expect(current_path).to eq(new_observation_path)
+    fill_in "Date", with: "01/01/2016"
+    select( student.full_name, from: 'observation_student_id' )
+    select( klass1.title, from: 'observation_subject')
+    choose('observation_ob_type_strength')
+    check('Check to make viewable to parents')
+    fill_in "Comment", with: "This is my comment"
+    click_on "Create Observation"
+
+    expect(current_path).to eq(observations_path)
+    new_observation = Observation.last
+    expect(new_observation.comment).to eq("This is my comment")
+    expect(new_observation.parent_viewable).to eq(true)
+    expect(new_observation.ob_type).to eq("Strength")
+    expect(new_observation.subject).to eq(klass1.title)
+    expect(new_observation.student_id).to eq(student.id)
+  end
+end
