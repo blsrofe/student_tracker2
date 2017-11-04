b82cc2d076ee0f2d899bfe913632a9533f24f5cf
Becki Srofe <blf_23@yahoo.com>
2017-11-04 11:16:01 -0600

    Add omniauth initializer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,15 +14,6 @@ class SessionsController < ApplicationController
     end
   end
 
-  #   @user = User.find_by(email: params[:session][:email])
-  #   if @user && @user.authenticate(params[:session][:password])
-  #     session[:user_id] = @user.id
-  #     redirect_to dashboard_path
-  #   else
-  #     redirect_to login_path
-  #   end
-  # end
-
   def destroy
     session.clear
     redirect_to root_path
