16ddd2c16cea3bf85c1568b5edc402f189989f3a
Becki Srofe <blf_23@yahoo.com>
2017-11-04 13:49:45 -0600

    Add sad path test for oauth

--- spec/features/users/user_logs_in_with_google_spec.rb
+++ spec/features/users/user_logs_in_with_google_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe "As a teacher" do
   it "I can log in with google if I have an account" do
     user = User.create!(first_name: "Jim", last_name: "Scott", email: "jim@gmail.com", password: "password")
-    
+
     stub_omniauth
 
     visit root_path
@@ -12,5 +12,15 @@ describe "As a teacher" do
     expect(current_path).to eq(dashboard_path)
     expect(page).to have_content("Logout")
     expect(page).to have_content("Welcome Jim")
+  end
+
+  it "I can not log in with google if I don't have an account" do
+    stub_omniauth
+
+    visit root_path
+    click_on "Sign in with Google"
+
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("Login Unsuccessful. Check with your administrator if you have not been provided with an account.")
   end
 end
