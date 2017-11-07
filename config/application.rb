af6e5ad3848931f0d1db7aa31477605ba840fd0d
Becki Srofe <blf_23@yahoo.com>
2017-11-07 15:02:26 -0700

    Add emails settings again

--- config/application.rb
+++ config/application.rb
@@ -34,8 +34,7 @@ module StudentTracker2
     }
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-  end
-end
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
