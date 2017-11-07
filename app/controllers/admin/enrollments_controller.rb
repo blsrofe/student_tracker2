6ff618195ae6e1ecb1ef9b76ce1402eef1394095
Becki Srofe <blf_23@yahoo.com>
2017-11-06 17:39:51 -0700

    Admin can drop students from class

--- app/controllers/admin/enrollments_controller.rb
+++ app/controllers/admin/enrollments_controller.rb
@@ -19,11 +19,18 @@ class Admin::EnrollmentsController < Admin::BaseController
     @enrollment = Enrollment.new(enrollment_params)
     if @enrollment.save
       flash[:message] = "Enrollment successfully created!"
-      redirect_to klasses_path
+      redirect_to admin_dashboard_path
     else
       flash[:message] = "Enrollment not saved. Make sure to fill in all fields"
       redirect_to new_admin_enrollment_path
     end
+  end
+
+  def destroy
+    @enrollment = Enrollment.find(params[:id])
+    @enrollment.destroy
+    flash[:message] = "Student has been dropped from class!"
+    redirect_to klasses_path
   end
 
   private
--- spec/features/admin/students/admin_can_enroll_students_spec.rb
+++ spec/features/admin/students/admin_can_enroll_students_spec.rb
@@ -7,19 +7,19 @@ describe "As an admin" do
     admin = User.create!(first_name: "Lynn", last_name: "Rivard", email: "lynn@gmail.com", password: "password", role: 1)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
-    visit klasses_path
+    visit admin_dashboard_path
 
     expect(student.klasses.count).to eq(0)
 
     click_on "Enroll Student"
 
     expect(current_path).to eq(new_admin_enrollment_path)
-
+    
     select("1", from: "enrollment_klass_id")
-    select("1", from: "enrollment_student_id")
+    select("Johnny1 Jones", from: "enrollment_student_id")
     click_on "Create Enrollment"
 
-    expect(current_path).to eq(klasses_path)
+    expect(current_path).to eq(admin_dashboard_path)
     expect(student.klasses.count).to eq(1)
   end
 end
